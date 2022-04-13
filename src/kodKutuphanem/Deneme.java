ac3b90bed662ddbf4f52c274531432fc08105f54
Ayhan Yazilim <yazilimdefteri1@gmail.com>
2022-04-14 02:11:24 +0300

    Bitirdigim Prıojeler

---
+++ src/kodKutuphanem/Deneme.java
@@ -0,0 +1,24 @@
+package kodKutuphanem;
+
+public class Deneme {
+    public static void main(String[] args) {
+
+
+        System.out.println("************************************************");
+        System.out.println("*   Basarili Market alış-veriş programı :-)    *\n");
+        System.out.println(" No \t   Ürün \t\t  Fiyat");
+        System.out.println("====\t =======\t \t=========");
+        System.out.println(" 01\t \t Domates   \t \t 2.10 TL");
+        System.out.println(" 02\t \t Patates   \t \t 3.20 TL");
+        System.out.println(" 03\t \t Biber     \t \t 1.50 TL");
+        System.out.println(" 04\t \t Soğan     \t \t 2.30 TL");
+        System.out.println(" 05\t \t Havuç     \t   \t 3.10 TL");
+        System.out.println(" 06\t \t Elma      \t   \t 1.20 TL");
+        System.out.println(" 07\t \t Muz     \t \t 1.90 TL");
+        System.out.println(" 08\t \t Çilek \t \t\t 6.10 TL");
+        System.out.println(" 09\t \t Kavun      \t 4.30 TL");
+        System.out.println(" 10\t \t Üzüm      \t \t 2.70 TL");
+        System.out.println(" 11\t \t Limon     \t \t 0.50 TL");
+        System.out.println("  0\t \t Cikis");
+    }
+}
